2d3905c5c6bcbd700bfdc5e38a9caed8eed9a1d0
MrJohnDowe <mrjohndowe@outlook.com>
2026-09-01 21:43:43 -0600

    Refactor database configuration and streamline PHP opening tag

--- config.php
+++ config.php
@@ -1,15 +1,12 @@
-<?php
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 const APP_NAME = 'Resume 2026';
 const APP_TIMEZONE = 'America/Denver';
-
 const DB_HOST = 'localhost';
 const DB_PORT = '3306';
-const DB_NAME = 'resume2026';
+const DB_NAME = 'resume';
 const DB_USER = 'resume';
-const DB_PASS = '8Cpl0DTD*Wsu[i[!';
-
+const DB_PASS = 'resume';
 const SESSION_NAME = 'resume2026';
 const MAX_PHOTO_BYTES = 5 * 1024 * 1024;
 
